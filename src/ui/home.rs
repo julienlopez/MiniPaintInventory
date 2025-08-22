712cff0f2618fb3bbb17fd39397a812f580592be
Julien Lopez <julien.lopez51@gmail.com>
2025-08-22 12:03:26 +0200

    starting to setup the add paint to box popup

--- src/ui/home.rs
+++ src/ui/home.rs
@@ -2,9 +2,15 @@
 
 use dioxus::prelude::*;
 
-use crate::{models::StorageBox, server_functions::get_boxes};
+use crate::{
+    models::StorageBox,
+    server_functions::{get_box_with_content, get_boxes},
+};
 
 use dioxus::logger::tracing::info;
+
+use dioxus_free_icons::icons::md_navigation_icons::MdClose;
+use dioxus_free_icons::Icon;
 
 #[component]
 pub fn Home() -> Element {
@@ -53,6 +59,7 @@ fn BoxList() -> Element {
 #[component]
 fn BoxSummary(r#box: StorageBox) -> Element {
     let mut collapsed = use_signal(|| true);
+    let mut show_add_paint_to_box = use_signal(|| false);
     rsx!(
         div { class: "box",
             div {
@@ -65,9 +72,9 @@ fn BoxSummary(r#box: StorageBox) -> Element {
                 div { class: "box_capacity", "0 / {r#box.capacity}" }
                 div {
                     class: "add_paint_to_box_button",
-                    onclick: |event| {
+                    onclick: move |event| {
                         event.stop_propagation();
-                        info!("plop");
+                        show_add_paint_to_box.set(true);
                     },
                     "+"
                 }
@@ -82,8 +89,59 @@ fn BoxSummary(r#box: StorageBox) -> Element {
             div {
                 class: "box_body",
                 style: if collapsed() { "display: none;" } else { "display: block;" },
-                "Blah"
+                BoxContent { box_id: r#box.id }
+            }
+            if show_add_paint_to_box() {
+                AddPaintToBoxPopup { show_add_paint_to_box, r#box: r#box.clone() }
             }
         }
     )
+}
+
+#[component]
+fn AddPaintToBoxPopup(show_add_paint_to_box: Signal<bool>, r#box: StorageBox) -> Element {
+    rsx!(
+        div { class: "add_paint_to_box_modal",
+            div { class: "modal_background" }
+            div { class: "add_paint_to_box_modal_content",
+                div {
+                    class: "add_paint_to_box_modal_close",
+                    onclick: move |_| {
+                        show_add_paint_to_box.set(false);
+                    },
+                    Icon { icon: MdClose }
+                }
+                div { class: "add_paint_to_box_form",
+                    div { "Add paint to : {r#box.name}" }
+                    div { "Paints" }
+                    div {
+                        class: "confirm_add_paint_to_box_button",
+                        onclick: move |_| {
+                            info!("Add paint to box ");
+                            show_add_paint_to_box.set(false);
+                        },
+                        "Add"
+                    }
+                }
+
+            }
+        }
+    )
+}
+#[component]
+fn BoxContent(box_id: i32) -> Element {
+    let content = use_resource(move || get_box_with_content(box_id));
+    match &*content.read_unchecked() {
+        Some(Ok(content)) => {
+            rsx!(div {
+                class: "box_content"
+            })
+        }
+        Some(Err(_)) => {
+            rsx! { "Error" }
+        }
+        None => {
+            rsx! { "..." }
+        }
+    }
 }
